99f7119e95b41b4ddf465fa172a9473cd969c2e7
Tony Narlock <tony@t420.(none)>
2011-04-21 21:44:07 -0500

    Newest koans

--- python 2/koans/about_iteration.py
+++ python 2/koans/about_iteration.py
@@ -54,12 +54,12 @@ class AboutIteration(Koan):
     
         # NOTE This still iterates through the whole names, so not particularly
         # efficient
-        self.assertEqual([__], filter(is_big_name, names)[:1])
+        self.assertEqual(['Clarence'], filter(is_big_name, names)[:1])
         
         # Boring but effective
         for item in names:
             if is_big_name(item):
-                self.assertEqual(__, item)
+                self.assertEqual('Clarence', item)
                 break
 
     # ------------------------------------------------------------------
@@ -72,10 +72,10 @@ class AboutIteration(Koan):
         
     def test_reduce_will_blow_your_mind(self):        
         result = reduce(self.add, [2, 3, 4]) 
-        self.assertEqual(__, result)
+        self.assertEqual(9, result)
     
         result2 = reduce(self.multiply, [2, 3, 4], 1) 
-        self.assertEqual(__, result2)
+        self.assertEqual(24, result2)
     
         # Extra Credit:
         # Describe in your own words what reduce does.
@@ -87,21 +87,21 @@ class AboutIteration(Koan):
         
         comprehension = [delicacy.capitalize() for delicacy in feast]
         
-        self.assertEqual(__, comprehension[0])
-        self.assertEqual(__, comprehension[2])
+        self.assertEqual("Lambs", comprehension[0])
+        self.assertEqual("Orangutans", comprehension[2])
         
     def test_use_pass_for_iterations_with_no_body(self):
         for num in range(1,5):
             pass
                 
-        self.assertEqual(__, num)
+        self.assertEqual(4, num)
         
     # ------------------------------------------------------------------
         
     def test_all_iteration_methods_work_on_any_sequence_not_just_lists(self):
         # Ranges are an iteratable sequence
         result = map(self.add_ten, range(1,4))
-        self.assertEqual(__, list(result))
+        self.assertEqual([11, 12, 13], list(result))
 
         try:
             # Files act like a collection of lines
@@ -109,7 +109,7 @@ class AboutIteration(Koan):
     
             def make_upcase(line) : return line.strip().upper()
             upcase_lines = map(make_upcase, file.readlines())
-            self.assertEqual(__, list(upcase_lines))
+            self.assertEqual(['I WOULD FALL', 'INTO THE SKY', 'DO YOU THINK TIME', 'WOULD PASS ME BY?'], list(upcase_lines))
             
             # NOTE: You can create your own collections that work with each,
             # map, select, etc.
